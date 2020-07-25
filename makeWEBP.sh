122a87d945a254ff1e9c7c6b9ceaf349316f0baf
nibmz7 <nibmz7@gmail.com>
2020-07-25 20:37:44 +0800

    Reorganize asset files

--- makeWEBP.sh
+++ makeWEBP.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR="$1"
-for file in "$DIR"/*
+for file in "$DIR"/assets/png/*
 do    
   name=$(basename "$file")
   if [[ $name == *.png ]]; then
-    cwebp -q 60 "$DIR/$name" -o "$DIR/${name%%.*}".webp
+    cwebp -q 60 "$DIR/assets/png/$name" -o "$DIR/assets/webp/${name%%.*}".webp
     echo "converted ${name}"
     fi
 done
