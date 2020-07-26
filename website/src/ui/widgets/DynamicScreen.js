fccd9b91d302a66468d79e545f7d5a52d33009ed
nibmz7 <nibmz7@gmail.com>
2020-07-26 21:13:18 +0800

    Created dynamic screen component II

--- website/src/ui/widgets/DynamicScreen.js
+++ website/src/ui/widgets/DynamicScreen.js
@@ -6,9 +6,6 @@ const template = `
             height: 100%;
             width: 200%;
             overflow-x: hidden;
-        }
-
-        #root.animatable {
             transition: transform .3s ease-out;
         }
 
@@ -25,6 +22,10 @@ const template = `
             background: green;
         }
 
+        .expand {
+            transform: translateX(-100vw);
+        }
+
         @media all and (min-width: 50rem) {
             #root {
                 width: 50rem;
@@ -33,10 +34,13 @@ const template = `
                 transform: translateX(15rem);
             }
             #list {
-                width: 40%;
+                width: 20rem;
             }
             #detail {
-                width: 60%;
+                width: 30rem;
+            }
+            #root.expand {
+                transform: translateX(0);
             }
         }
 
@@ -46,6 +50,7 @@ const template = `
         <div id="list" class="fragment"></div>
         <div id="detail" class="fragment"></div>
     </div>
+
 `;
 
 export default class DynamicScreen extends HTMLElement {
@@ -53,6 +58,36 @@ export default class DynamicScreen extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = template;
+    this.root = this.shadowRoot.getElementById('root');
+    this.list = this.shadowRoot.getElementById('list');
+    this.detail = this.shadowRoot.getElementById('detail');
+    this.hasExpanded = false;
+    this.listFragment = null;
+    this.detailFragment = null;
+  }
+
+  showList(fragment) {
+    this.listFragment = fragment;
+    this.list.appendChild(fragment);
+  }
+
+  showDetail(fragment) {
+    if (!this.hasExpanded) {
+      this.hasExpanded = true;
+      this.root.classList.add('expand');
+    }
+    if(this.detailFragment) this.detailFragment.remove();
+    this.detail.appendChild(fragment);
+    this.detailFragment = fragment;
+  }
+
+  closeDetail() {
+    if (this.hasExpanded) {
+      this.hasExpanded = false;
+      this.root.classList.remove('expand');
+    }
+    this.detailFragment.remove();
+    this.detailFragment = null;
   }
 }
 
