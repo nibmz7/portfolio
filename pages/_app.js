48a12d92bb525135617407e5c0f99df6a10b1f1a
Nur Ilyas <nibmz7@gmail.com>
2024-01-02 00:27:53 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,52 +1,79 @@
-import { useRouter } from "next/router";
-import { MDXProvider } from "@mdx-js/react";
-import { AnimatePresence, motion } from "framer-motion";
-import { markdownComponents } from "../lib/markdownComponents";
-import "../styles/global.css";
-import ArrowBack from "../components/icons/arrowBack";
-import Link from "next/link";
-import Head from "next/head";
+import { useRouter } from 'next/router';
+import { MDXProvider } from '@mdx-js/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { markdownComponents } from '../lib/markdownComponents';
+import '../styles/global.css';
+import ArrowBack from '../components/icons/arrowBack';
+import Link from 'next/link';
+import Head from 'next/head';
+import { useEffect, useRef } from 'react';
 
 const fadeVariants = {
   inactive: {
     opacity: 0,
-    pointerEvents: "none",
+    pointerEvents: 'none',
   },
   active: {
     opacity: 1,
-    pointerEvents: "auto",
+    pointerEvents: 'auto',
   },
 };
 
 const titleVariants = {
   homeInactive: {
     y: -30,
-    pointerEvents: "none",
+    pointerEvents: 'none',
   },
   homeActive: {
     y: 0,
-    pointerEvents: "auto",
+    pointerEvents: 'auto',
   },
   postInactive: {
     y: 30,
-    pointerEvents: "none",
+    pointerEvents: 'none',
   },
   postActive: {
     y: 0,
-    pointerEvents: "auto",
+    pointerEvents: 'auto',
   },
 };
 
 const titleTransition = {
-  type: "tween",
+  type: 'tween',
   duration: 0.5,
 };
 
-const HOVER_BG_COLOR = "#f0f9ff";
+const HOVER_BG_COLOR = '#f0f9ff';
 
 export default function App({ Component, pageProps }) {
   const { asPath } = useRouter();
-  const isHome = asPath === "/";
+  const isHome = asPath === '/';
+
+  const isHomeRef = useRef(isHome);
+  const containerRef = useRef(null);
+  const homeScrollPositionRef = useRef(0);
+  isHomeRef.current = isHome;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (isHomeRef.current) {
+        homeScrollPositionRef.current = containerRef.current.scrollTop;
+      }
+    };
+
+    containerRef.current.addEventListener('scroll', handleScroll, {
+      passive: true,
+    });
+
+    return () =>
+      containerRef.current.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    const scrollPos = isHome ? homeScrollPositionRef.current : 0;
+    containerRef.current.scrollTo({ top: scrollPos, behavior: 'smooth' });
+  }, [isHome]);
+
   return (
     <MDXProvider components={markdownComponents}>
       <Head>
@@ -70,15 +97,18 @@ export default function App({ Component, pageProps }) {
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="description" content="Nur Ilyas Blog" />
       </Head>
-      <main className="h-screen w-screen flex flex-col items-center overflow-y-auto max-h-screen">
+      <main
+        ref={containerRef}
+        className="h-screen w-screen flex flex-col items-center overflow-y-auto max-h-screen"
+      >
         <header className="w-full max-w-md mt-4">
           <div className="shadow flex rounded-md bg-white p-2 mx-4">
             <div className="overflow-hidden relative w-full p-2">
               <motion.h1
                 key="home-title"
                 className="font-semibold text-slate-700 absolute"
-                initial={isHome ? 'homeActive': 'homeInactive'}
-                animate={isHome ? 'homeActive': 'homeInactive'}
+                initial={isHome ? 'homeActive' : 'homeInactive'}
+                animate={isHome ? 'homeActive' : 'homeInactive'}
                 variants={titleVariants}
                 transition={titleTransition}
               >
@@ -86,15 +116,15 @@ export default function App({ Component, pageProps }) {
               </motion.h1>
               <motion.div
                 key="blog-title absolute"
-                initial={!isHome ? 'postActive': 'postInactive'}
-                animate={!isHome ? 'postActive': 'postInactive'}
+                initial={!isHome ? 'postActive' : 'postInactive'}
+                animate={!isHome ? 'postActive' : 'postInactive'}
                 variants={titleVariants}
                 transition={titleTransition}
               >
                 <Link href="/">
                   <motion.div
                     className="flex items-center cursor-pointer w-max rounded-md -ml-1 pl-1 pr-2"
-                    initial={{ backgroundColor: "#ffffff" }}
+                    initial={{ backgroundColor: '#ffffff' }}
                     whileHover={{ backgroundColor: HOVER_BG_COLOR }}
                     whileTap={{ backgroundColor: HOVER_BG_COLOR }}
                   >
